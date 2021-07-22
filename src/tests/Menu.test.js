ce9386979aef66466895a46c1116f2db06022d3e
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-07-22 15:03:02 +0300

    Update Menu.test.js

--- src/tests/Menu.test.js
+++ src/tests/Menu.test.js
@@ -5,6 +5,7 @@ test('renders menu page', () => {
     render(<Menu />);
     expect(screen.getByTestId('menu')).toBeInTheDocument();
     expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(screen.getByText('Minesweeper')).toBeInTheDocument();
     expect(screen.getByLabelText('Width')).toBeInTheDocument();
     expect(screen.getByLabelText('Height')).toBeInTheDocument();
     expect(screen.getByLabelText('Mines')).toBeInTheDocument();
