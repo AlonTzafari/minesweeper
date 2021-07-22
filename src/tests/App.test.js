b2fae5448746b7b97e5ea82335dabe1ec7a1e0b9
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-07-22 13:21:37 +0300

    export menu

--- src/tests/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders action menu', () => {
-    render(<App />);
-    expect(screen.getByTestId('actionMenu')).toBeInTheDocument();
-});
--- src/tests/Menu.test.js
+++ src/tests/Menu.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Menu from '../pages/Menu';
 
-test('renders action menu', () => {
+test('renders menu page', () => {
     render(<Menu />);
     expect(screen.getByTestId('menu')).toBeInTheDocument();
 });
