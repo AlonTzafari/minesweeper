05c250d0bd665ceb709a7eae157d4dae07728146
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-07-24 18:45:31 +0300

    code cleanup

--- src/logic/Game.js
+++ src/logic/Game.js
@@ -10,30 +10,30 @@ export default class Game {
     noFlags;
 
     constructor(config) {
-        const {width, height, mines, superman} = config;
-        
+        const { width, height, mines, superman } = config;
+
         this.flagsLeft = mines;
         this.superman = superman;
         this.minesLeft = mines;
 
-        this.board = createMatrix(width, height, () => {return {state:'hidden', value: 0}});
+        this.board = createMatrix(width, height, () => {
+            return { state: "hidden", value: 0 };
+        });
         const mineLocations = this.placeMinesRandomly(mines);
         this.calculateTileValues(mineLocations);
-
     }
 
     reveal(x, y) {
-
         const tile = this.board[y][x];
-        if (tile.state === 'visible' || tile.state === 'flag') return;
+        if (tile.state === "visible" || tile.state === "flag") return;
 
-        if (tile.value === 'bomb') {
-            tile.state = 'visible';
-            this.onLose();  
-        } else if (typeof tile.value ===  'number' && tile.value > 0) {
-            tile.state = 'visible';
+        if (tile.value === "bomb") {
+            tile.state = "visible";
+            this.onLose();
+        } else if (typeof tile.value === "number" && tile.value > 0) {
+            tile.state = "visible";
         } else {
-            const stack = [[x,y]];
+            const stack = [[x, y]];
             while (stack.length > 0) {
                 const args = stack.pop();
                 floodReveal(this.board, ...args, stack);
@@ -43,28 +43,26 @@ export default class Game {
         function floodReveal(board, x, y, stack) {
             if (x < 0 || x >= board[0].length) return;
             if (y < 0 || y >= board.length) return;
-            const tile = board[y][x]; 
-            if (tile.value === 'bomb') return;
-            if (typeof tile.value ===  'number' && tile.value > 0) {
-                tile.state = 'visible';
+            const tile = board[y][x];
+            if (tile.value === "bomb") return;
+            if (typeof tile.value === "number" && tile.value > 0) {
+                tile.state = "visible";
                 return;
             }
-            if(tile.state === 'visible') return;
-            tile.state = 'visible';
+            if (tile.state === "visible") return;
+            tile.state = "visible";
             stack.push([x - 1, y]);
             stack.push([x + 1, y]);
             stack.push([x, y - 1]);
             stack.push([x, y + 1]);
         }
-        
     }
 
     flag(x, y) {
-
         const tile = this.board[y][x];
-        if (tile.state === 'visible') return;
-        if (tile.state === 'flag') {
-            tile.state = 'hidden';
+        if (tile.state === "visible") return;
+        if (tile.state === "flag") {
+            tile.state = "hidden";
             this.flagsLeft++;
             return;
         }
@@ -72,11 +70,11 @@ export default class Game {
             this.noFlags();
             return;
         }
-        tile.state = 'flag';
+        tile.state = "flag";
         this.flagsLeft--;
-        if (tile.value === 'bomb') {
+        if (tile.value === "bomb") {
             this.minesLeft--;
-            if(this.minesLeft === 0) this.onWin();
+            if (this.minesLeft === 0) this.onWin();
         }
     }
 
@@ -86,11 +84,11 @@ export default class Game {
         while (minesToPlace > 0) {
             const x = Math.floor(Math.random() * this.board[0].length);
             const y = Math.floor(Math.random() * this.board.length);
-            const tile = this.board[y][x]; 
-            if(tile.value === 'bomb') continue;
-            this.board[y][x].value = 'bomb';
+            const tile = this.board[y][x];
+            if (tile.value === "bomb") continue;
+            this.board[y][x].value = "bomb";
             minesToPlace--;
-            mineLocations.push({x,y});
+            mineLocations.push({ x, y });
         }
         return mineLocations;
     }
@@ -100,27 +98,26 @@ export default class Game {
         const width = this.board[0].length;
 
         const tilesAround = [
-            {x: -1, y: -1},
-            {x: 0, y: -1},
-            {x: 1, y: -1},
-            {x: -1, y: 0},
-            {x: 1, y: 0},
-            {x: -1, y: 1},
-            {x: 0, y: 1},
-            {x: 1, y: 1},
+            { x: -1, y: -1 },
+            { x: 0, y: -1 },
+            { x: 1, y: -1 },
+            { x: -1, y: 0 },
+            { x: 1, y: 0 },
+            { x: -1, y: 1 },
+            { x: 0, y: 1 },
+            { x: 1, y: 1 },
         ];
 
-        for(const minePos of mineLocations) {    
+        for (const minePos of mineLocations) {
             for (const tilePos of tilesAround) {
                 const x = tilePos.x + minePos.x;
                 const y = tilePos.y + minePos.y;
                 if (x < 0 || x >= width) continue;
                 if (y < 0 || y >= height) continue;
                 const tileAtPos = this.board[y][x];
-                if (tileAtPos.value === 'bomb') continue;
+                if (tileAtPos.value === "bomb") continue;
                 tileAtPos.value++;
             }
         }
     }
-    
 }
--- src/logic/createMatrix.js
+++ src/logic/createMatrix.js
@@ -1,5 +1,5 @@
 export default function createMatrix(width, height, fillValue) {
-    return new Array(height).fill(0).map( () => {
+    return new Array(height).fill(0).map(() => {
         const widthArr = new Array(width).fill(0);
         return widthArr.map(() => {
             return fillValue();
